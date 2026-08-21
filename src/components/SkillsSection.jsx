b8dfbe26f6dbba3e8aa97d5e80b29651347f9e98
pealbert <pealbert@email.cz>
2026-08-21 10:33:29 +0200

    Create Projects Section

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -32,7 +32,7 @@ export const SkillsSection = () => {
 	const filteredSkills = skills.filter((skill) => activeCategory === "all" || skill.category === activeCategory);
 
 	return (
-		<section id="#skills" className="py-24 px-4 relative bg-secondary/30">
+		<section id="skills" className="py-24 px-4 relative bg-secondary/30">
 			<div className="container mx-auto max-w-5xl">
 				<h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
 					My <span className="text-primary">Skills</span>
